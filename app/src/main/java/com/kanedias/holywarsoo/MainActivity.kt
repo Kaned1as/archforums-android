22380520375832282885c9a5020c053a80618b57
Kanedias <kanedias@xaker.ru>
2020-01-12 23:42:40 +0300

    Add release notes

--- app/src/main/java/com/kanedias/holywarsoo/MainActivity.kt
+++ app/src/main/java/com/kanedias/holywarsoo/MainActivity.kt
@@ -171,7 +171,8 @@ class MainActivity : AppCompatActivity() {
             5 to Release("1.1.3", R.string.release_5),
             6 to Release("1.1.4", R.string.release_6),
             7 to Release("1.1.5", R.string.release_7),
-            8 to Release("1.2.0", R.string.release_8)
+            8 to Release("1.2.0", R.string.release_8),
+            9 to Release("1.2.1", R.string.release_9)
         )
 
         if (Config.lastVersion == 0) {
