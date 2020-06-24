Kaned1as/holywarsoo-android

Add ability to add predefined smilies

app/src/main/java/com/kanedias/holywarsoo/EditorViews.kt
@@ -7,15 +7,23 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.TypedValue
+import android.view.Gravity
 import androidx.core.content.ContextCompat
 import android.view.View
+import android.view.WindowManager
 import android.widget.*
 import androidx.fragment.app.Fragment
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
+import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.kanedias.holywarsoo.misc.resolveAttr
 import com.kanedias.holywarsoo.service.Network
+import com.kanedias.holywarsoo.service.SmiliesCache
 import kotlinx.coroutines.*
 
 /**
@@ -40,6 +48,9 @@ class EditorViews(private val parent: Fragment, private val iv: View) {
 
     @BindView(R.id.edit_quick_image)
     lateinit var imageUpload: ImageView
+
+    @BindView(R.id.edit_quick_smilies)
+    lateinit var addSmilie: ImageView
 
     @BindView(R.id.edit_quick_button_area)
     lateinit var buttonArea: GridLayout
@@ -91,6 +102,34 @@ class EditorViews(private val parent: Fragment, private val iv: View) {
         }
 
         clipboardSwitch.isChecked = false
+    }
+
+    @OnClick(R.id.edit_quick_smilies)
+    fun insertSmilie(clicked: View) {
+        val smilieTypes = SmiliesCache.getAllSmilies()
+
+        val smiliesView = View.inflate(clicked.context, R.layout.view_smilies_panel, null)
+        val smiliesTable = smiliesView.findViewById<GridLayout>(R.id.smilies_table)
+        val pw = PopupWindow().apply {
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+            width = WindowManager.LayoutParams.WRAP_CONTENT
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            contentView = smiliesView
+            isOutsideTouchable = true
+        }
+
+        for (smilie in smilieTypes) {
+            smiliesTable.addView(ImageView(clicked.context).apply {
+                setImageDrawable(smilie.value)
+                if (smilie.value is GifDrawable)
+                    (smilie.value as GifDrawable).start()
+                setOnClickListener {
+                    insertInCursorPosition("", smilie.key)
+                    pw.dismiss()
+                }
+            })
+        }
+        pw.showAsDropDown(clicked, 0, 0, Gravity.TOP)
     }
 
     /**

app/src/main/java/com/kanedias/holywarsoo/MainApplication.kt
@@ -10,6 +10,7 @@ import org.acra.annotation.AcraDialog
 import org.acra.annotation.AcraMailSender
 import org.acra.data.StringFormat
 import com.kanedias.holywarsoo.service.Network
+import com.kanedias.holywarsoo.service.SmiliesCache
 
 
 /**
@@ -38,5 +39,6 @@ class MainApplication : Application() {
         Config.init(this)
         Network.init(this)
         Database.init(this)
+        SmiliesCache.init(this)
     }
 }

app/src/main/java/com/kanedias/holywarsoo/service/SmiliesCache.kt
@@ -1,0 +1,58 @@
+package com.kanedias.holywarsoo.service
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.kanedias.holywarsoo.markdown.GlideGifSupportStore
+import okhttp3.HttpUrl
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
+
+/**
+ * Tracks available smilies on a service.
+ * Smilies are specific for each forum instance.
+ *
+ * @author Kanedias
+ *
+ * Created on 21.06.20
+ */
+object SmiliesCache {
+
+    const val SMILIES_PREFIX = "/img/smilies"
+
+    private val cache = HashMap<String, Drawable>(512)
+
+    fun init(ctx: Context) {
+        val smiliesTxt = ctx.assets.open("smile-mappings.json").readBytes().toString(StandardCharsets.UTF_8)
+        val smiliesJson = JSONObject(smiliesTxt)
+        for (key in smiliesJson.keys()) {
+            val smilieUrl = Network.resolve("$SMILIES_PREFIX/${smiliesJson.getString(key)}") ?: continue
+            putSmilie(ctx, key, smilieUrl)
+        }
+    }
+
+    fun putSmilie(ctx: Context, key: String, address: HttpUrl) {
+        Glide.with(ctx)
+            .asDrawable()
+            .transform(GlideGifSupportStore.ScaleToDensity(ctx))
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .load(address.toString())
+            .into(object: CustomTarget<Drawable>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                    cache.put(key, resource)
+                }
+
+            })
+    }
+
+    fun containsKey(key: String) = cache.containsKey(key)
+
+    fun getSmilie(key: String) = cache[key]
+
+    fun getAllSmilies() = cache.clone() as Map<String, Drawable>
+}

Kaned1as/archforums-android

app/src/main/java/com/kanedias/archforums/EditorViews.kt
@@ -7,15 +7,23 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.TypedValue
+import android.view.Gravity
 import androidx.core.content.ContextCompat
 import android.view.View
+import android.view.WindowManager
 import android.widget.*
 import androidx.fragment.app.Fragment
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
+import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.kanedias.archforums.misc.resolveAttr
 import com.kanedias.archforums.service.Network
+import com.kanedias.archforums.service.SmiliesCache
 import kotlinx.coroutines.*
 
 /**
@@ -40,6 +48,9 @@ class EditorViews(private val parent: Fragment, private val iv: View) {
 
     @BindView(R.id.edit_quick_image)
     lateinit var imageUpload: ImageView
+
+    @BindView(R.id.edit_quick_smilies)
+    lateinit var addSmilie: ImageView
 
     @BindView(R.id.edit_quick_button_area)
     lateinit var buttonArea: GridLayout
@@ -91,6 +102,34 @@ class EditorViews(private val parent: Fragment, private val iv: View) {
         }
 
         clipboardSwitch.isChecked = false
+    }
+
+    @OnClick(R.id.edit_quick_smilies)
+    fun insertSmilie(clicked: View) {
+        val smilieTypes = SmiliesCache.getAllSmilies()
+
+        val smiliesView = View.inflate(clicked.context, R.layout.view_smilies_panel, null)
+        val smiliesTable = smiliesView.findViewById<GridLayout>(R.id.smilies_table)
+        val pw = PopupWindow().apply {
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+            width = WindowManager.LayoutParams.WRAP_CONTENT
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            contentView = smiliesView
+            isOutsideTouchable = true
+        }
+
+        for (smilie in smilieTypes) {
+            smiliesTable.addView(ImageView(clicked.context).apply {
+                setImageDrawable(smilie.value)
+                if (smilie.value is GifDrawable)
+                    (smilie.value as GifDrawable).start()
+                setOnClickListener {
+                    insertInCursorPosition("", smilie.key)
+                    pw.dismiss()
+                }
+            })
+        }
+        pw.showAsDropDown(clicked, 0, 0, Gravity.TOP)
     }
 
     /**

app/src/main/java/com/kanedias/archforums/MainApplication.kt
@@ -10,6 +10,7 @@ import org.acra.annotation.AcraDialog
 import org.acra.annotation.AcraMailSender
 import org.acra.data.StringFormat
 import com.kanedias.archforums.service.Network
+import com.kanedias.archforums.service.SmiliesCache
 
 
 /**
@@ -38,5 +39,6 @@ class MainApplication : Application() {
         Config.init(this)
         Network.init(this)
         Database.init(this)
+        SmiliesCache.init(this)
     }
 }

app/src/main/java/com/kanedias/archforums/service/SmiliesCache.kt
@@ -1,0 +1,58 @@
+package com.kanedias.archforums.service
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.kanedias.archforums.markdown.GlideGifSupportStore
+import okhttp3.HttpUrl
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
+
+/**
+ * Tracks available smilies on a service.
+ * Smilies are specific for each forum instance.
+ *
+ * @author Kanedias
+ *
+ * Created on 21.06.20
+ */
+object SmiliesCache {
+
+    const val SMILIES_PREFIX = "/img/smilies"
+
+    private val cache = HashMap<String, Drawable>(512)
+
+    fun init(ctx: Context) {
+        val smiliesTxt = ctx.assets.open("smile-mappings.json").readBytes().toString(StandardCharsets.UTF_8)
+        val smiliesJson = JSONObject(smiliesTxt)
+        for (key in smiliesJson.keys()) {
+            val smilieUrl = Network.resolve("$SMILIES_PREFIX/${smiliesJson.getString(key)}") ?: continue
+            putSmilie(ctx, key, smilieUrl)
+        }
+    }
+
+    fun putSmilie(ctx: Context, key: String, address: HttpUrl) {
+        Glide.with(ctx)
+            .asDrawable()
+            .transform(GlideGifSupportStore.ScaleToDensity(ctx))
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .load(address.toString())
+            .into(object: CustomTarget<Drawable>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                    cache.put(key, resource)
+                }
+
+            })
+    }
+
+    fun containsKey(key: String) = cache.containsKey(key)
+
+    fun getSmilie(key: String) = cache[key]
+
+    fun getAllSmilies() = cache.clone() as Map<String, Drawable>
+}